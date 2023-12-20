a7beec305d279e6f767a1ce1240eec65f826c02e
Shaamallow <eyal.benaroche@gmail.com>
2023-12-20 23:20:30 +0100

    correlation map
    TODO : features density

--- src/routes/features/+page.svelte
+++ src/routes/features/+page.svelte
@@ -8,47 +8,60 @@
 	let divCorrelationMap: HTMLDivElement;
 	let divTSNE: HTMLDivElement;
 
-	let features_div: HTMLDivElement;
+	let divFeatures: HTMLDivElement;
 
-	let data_original: any;
-	let data_pca: any;
-	let data_tsne: any;
-	let data_correlation_map: any;
+	let dataOriginal: any;
+	let dataPCA: any;
+	let dataTSNE: any;
+	let dataCorrelationMap: any;
 
 	onMount(() => {
 		loaded = true; // this is for the fade transition
 
+		const parseValuesToFloat = (obj: Record<string, string | number>): Record<string, number> => {
+			const result: Record<string, number> = {};
+
+			for (const key in obj) {
+				if (obj.hasOwnProperty(key)) {
+					// Parse the value to float if it's a string
+					result[key] =
+						typeof obj[key] === 'string' ? parseFloat(obj[key] as string) : (obj[key] as number);
+				}
+			}
+
+			return result;
+		};
 		// Now load the dataset only ONCE and store it in the variables
 		// to be used in the draw() functions later on and avoid loading
 		// the dataset every time the window is resized (testing purposes especially...)
 		d3.csv('/dataset.csv').then((data) => {
 			console.log('data_original');
-			data_original = data;
+			// Parse the values to float
+			dataOriginal = data.map(parseValuesToFloat);
 		});
 		d3.csv('/dataset_pca.csv').then((data) => {
-			console.log('data_pca');
-			data_pca = [];
+			console.log('data_pca loaded');
+			dataPCA = [];
 			let nRows = data.length;
 			for (let i = 0; i < nRows; i++) {
 				let row = data[i];
-				data_pca.push({
+				dataPCA.push({
 					PC1: parseFloat(row.PC1),
 					PC2: parseFloat(row.PC2),
 					id: row.id,
 					diagnosis: parseInt(row.diagnosis)
 				});
 			}
-			console.log(data_pca);
 
 			drawPCA();
 		});
 		d3.csv('/dataset_tsne.csv').then((data) => {
-			console.log('data_tsne');
-			data_tsne = [];
+			console.log('data_tsne loaded');
+			dataTSNE = [];
 			let nRows = data.length;
 			for (let i = 0; i < nRows; i++) {
 				let row = data[i];
-				data_tsne.push({
+				dataTSNE.push({
 					x: parseFloat(row.X),
 					y: parseFloat(row.Y),
 					id: row.id,
@@ -56,12 +69,18 @@
 				});
 			}
 
-			console.log(data_tsne);
 			drawTSNE();
 		});
 		d3.csv('/dataset_correlation_map.csv').then((data) => {
-			data_correlation_map = data;
 			console.log('data_correlation_map');
+
+			dataCorrelationMap = data.map(parseValuesToFloat);
+
+			// remove 1st column of each row
+			dataCorrelationMap.forEach((row: any) => {
+				delete row[''];
+			});
+			drawCorrelationMap();
 		});
 
 		window.addEventListener('resize', draw);
@@ -71,7 +90,7 @@
 		console.log('draw');
 
 		// empty the corresponding svg elements
-		d3.select(features_div).html(null);
+		//d3.select(divFeatures).html(null);
 		d3.select(divPCA).html(null);
 		d3.select(divCorrelationMap).html(null);
 		d3.select(divTSNE).html(null);
@@ -95,16 +114,16 @@
 
 		let margin = 50;
 
-		let min_value_PC1 = d3.min(data_pca, (d: any) => d.PC1 as number) as number;
-		let max_value_PC1 = d3.max(data_pca, (d: any) => d.PC1 as number) as number;
+		let min_value_PC1 = d3.min(dataPCA, (d: any) => d.PC1 as number) as number;
+		let max_value_PC1 = d3.max(dataPCA, (d: any) => d.PC1 as number) as number;
 
 		const xScale = d3
 			.scaleLinear()
 			.domain([min_value_PC1 * 1.1, max_value_PC1 * 1.1])
 			.range([margin, width - margin]);
 
-		let min_value_PC2 = d3.min(data_pca, (d: any) => d.PC2 as number) as number;
-		let max_value_PC2 = d3.max(data_pca, (d: any) => d.PC2 as number) as number;
+		let min_value_PC2 = d3.min(dataPCA, (d: any) => d.PC2 as number) as number;
+		let max_value_PC2 = d3.max(dataPCA, (d: any) => d.PC2 as number) as number;
 
 		const yScale = d3
 			.scaleLinear()
@@ -145,7 +164,7 @@
 		svg
 			.append('g')
 			.selectAll('circle')
-			.data(data_pca)
+			.data(dataPCA)
 			.enter()
 			.append('circle')
 			.attr('cx', (d: any) => xScale(d.PC1))
@@ -205,16 +224,16 @@
 
 		let margin = 50;
 
-		let min_value_x = d3.min(data_tsne, (d: any) => d.x as number) as number;
-		let max_value_x = d3.max(data_tsne, (d: any) => d.x as number) as number;
+		let min_value_x = d3.min(dataTSNE, (d: any) => d.x as number) as number;
+		let max_value_x = d3.max(dataTSNE, (d: any) => d.x as number) as number;
 
 		const xScale = d3
 			.scaleLinear()
 			.domain([min_value_x * 1.1, max_value_x * 1.1])
 			.range([margin, width - margin]);
 
-		let min_value_y = d3.min(data_tsne, (d: any) => d.y as number) as number;
-		let max_value_y = d3.max(data_tsne, (d: any) => d.y as number) as number;
+		let min_value_y = d3.min(dataTSNE, (d: any) => d.y as number) as number;
+		let max_value_y = d3.max(dataTSNE, (d: any) => d.y as number) as number;
 
 		const yScale = d3
 			.scaleLinear()
@@ -255,7 +274,7 @@
 		svg
 			.append('g')
 			.selectAll('circle')
-			.data(data_tsne)
+			.data(dataTSNE)
 			.enter()
 			.append('circle')
 			.attr('cx', (d: any) => xScale(d.x))
@@ -306,7 +325,97 @@
 			.style('text-anchor', 'middle')
 			.attr('class', 'font-display text-3xl font-bold fill-current');
 	}
-	function drawCorrelationMap(): void {}
+	function drawCorrelationMap(): void {
+		// Correlation Map function
+
+		// get the width of parent element
+		let width = divCorrelationMap.clientWidth;
+		let height = width; // to make it almost a square
+
+		let margin = 10;
+		let margin_right = 50;
+
+		let nRows = dataCorrelationMap.length;
+		let keys = Object.keys(dataCorrelationMap[0]);
+		let nCols = keys.length;
+
+		let min_value = d3.min(dataCorrelationMap, (d: any) => {
+			let min = d3.min(Object.values(d), (d: any) => d as number);
+			return min;
+		}) as number;
+
+		let max_value = d3.max(dataCorrelationMap, (d: any) => {
+			let max = d3.max(Object.values(d), (d: any) => d as number);
+			return max;
+		}) as number;
+
+		let colorScale = d3.scaleSequential(d3.interpolateViridis).domain([min_value, max_value]);
+
+		let xScale = d3
+			.scaleBand()
+			.range([margin + width * 0.2, width - margin_right])
+			.domain(d3.range(nCols) as any);
+
+		let yScale = d3
+			.scaleBand()
+			.range([margin + height * 0.1, height * 0.85 - margin])
+			.domain(d3.range(nRows) as any);
+
+		const svg = d3
+			.select(divCorrelationMap)
+			.append('svg')
+			.attr('width', width)
+			.attr('height', height);
+
+		let xAxis = d3.axisBottom(xScale).tickFormat((d: any) => keys[d]);
+		let yAxis = d3.axisLeft(yScale).tickFormat((d: any) => keys[d]);
+
+		// Now add the squares for the correlation map
+		// each row of the dataset is a row of squares
+		// each square is a rect element
+		svg
+			.selectAll()
+			.data(
+				dataCorrelationMap.flatMap((row: any, i: any) =>
+					Object.entries(row).map(([key, value], j) => ({ i, j, value }))
+				)
+			)
+			.enter()
+			.append('rect')
+			.attr('x', (d: any) => xScale(d.j) as number)
+			.attr('y', (d: any) => yScale(d.i) as number)
+			.attr('width', xScale.bandwidth())
+			.attr('height', yScale.bandwidth())
+			.style('fill', (d: any) => colorScale(d.value));
+
+		// Add X axis
+		svg
+			.append('g')
+			.attr('transform', `translate(0, ${height * 0.85 - margin / 2})`)
+			.call(xAxis)
+			.selectAll('text')
+			.attr('transform', 'translate(-10,0)rotate(-45)')
+			.attr('class', 'fill-current')
+			.style('text-anchor', 'end');
+
+		// Add Y axis
+		svg
+			.append('g')
+			.attr('transform', `translate(${margin / 2 + width * 0.2}, 0)`)
+			.call(yAxis)
+			.selectAll('text')
+			.attr('class', 'fill-current')
+			.style('text-anchor', 'end');
+
+		// add title
+		svg
+			.append('text')
+			.attr('x', '50%')
+			.attr('y', height * 0.07)
+			.text('Correlation Map')
+			.style('text-anchor', 'middle')
+			.attr('class', 'font-display text-3xl font-bold fill-current');
+	}
 </script>
 
 <svelte:head>
@@ -337,10 +446,8 @@
 
 		<!-- PCA | Correlation Map | TSNE -->
 
-		<div
-			class="md:w-[50rem] rounded mx-auto bg-neutral flex flex-col lg:flex-row lg:flew-wrap justify-center items-center gap-5 p-2 mt-10"
-		>
-			<div class="w-full h-[30rem] bg-primary" bind:this={divCorrelationMap}></div>
+		<div class="container lg:w-[50rem] mx-auto flex justify-center items-center mt-10 p-2">
+			<div class="w-full m-2 bg-neutral rounded-md p-2" bind:this={divCorrelationMap}></div>
 		</div>
 
 		<div class="divider my-20" />
@@ -356,7 +463,7 @@
 
 		<div
 			class="mx-10 flex flex-col justify-center items-center gap-10"
-			bind:this={features_div}
+			bind:this={divFeatures}
 			id="features_div"
 		>
 			<div class="flex justify-center items-center gap-5">
